cb89451ee7932d392f3a0c82837213f50767badc
jack-edu <kill.j@northeastern.edu>
2025-09-24 13:04:41 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
-from routes.vehicle_routes import vehicle_bp
-from routes.analytics_routes import analytics_bp
-from utils.data_handler import load_vehicles, load_customers
+from utils.data_handler import load_vehicles, load_customers, vehicles
 
 app = Flask(__name__)
 CORS(app)
 
-## Register blueprints
-app.register_blueprint(vehicle_bp)
-app.register_blueprint(analytics_bp)
-
 if __name__ == "__main__":
     load_vehicles()
     load_customers()
+    
+    from routes.vehicle_routes import vehicle_bp
+    from routes.analytics_routes import analytics_bp
+    
+    app.register_blueprint(vehicle_bp)
+    app.register_blueprint(analytics_bp)
+    
     app.run(debug=True)
